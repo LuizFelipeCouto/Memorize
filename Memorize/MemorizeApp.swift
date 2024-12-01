f3fd2eab2efc5cc76947e5c114754edb2e1e641f
lfelipe.dev <129457138+LuizFelipeCouto@users.noreply.github.com>
2024-11-30 21:59:19 -0300

    Primeiro commit da criação do app de Memorizar cartas

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Luiz Felipe Aparecido do Couto on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
