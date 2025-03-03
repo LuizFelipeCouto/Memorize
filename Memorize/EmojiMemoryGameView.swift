005616d15d738fa481d6d788bbdfda30f0a3d20a
lfelipe.dev <129457138+LuizFelipeCouto@users.noreply.github.com>
2025-03-03 17:01:29 -0300

    Na 6 aula, fizemos a aspectRadio das cartas e melhoramos alguns codigos

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -16,12 +16,12 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
+    private let aspectRatio: CGFloat = 2/3
+    
     var body: some View {
         VStack{
-            ScrollView{
                 cards
                     .animation(.default, value: viewModel.cards)
-            }
             
             Button("Embaralhar") {
                 viewModel.shuffle()
@@ -30,21 +30,16 @@ struct EmojiMemoryGameView: View {
         .padding()
     }
     
-    var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach (viewModel.cards) { card in
-                CardView(card)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
-            }
+    private var cards: some View {
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
+                    CardView(card)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                }
         }
         .foregroundColor(Color.orange)
     }
-    
-    
 }
 
 struct CardView: View{
@@ -69,8 +64,8 @@ struct CardView: View{
                     .aspectRatio(contentMode: .fit)
             }
             .opacity(card.isFaceUp ? 1 : 0 )
-        base.fill()
-            .opacity(card.isFaceUp ? 0 : 1 )
+            base.fill()
+                .opacity(card.isFaceUp ? 0 : 1 )
             
         }
         .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
